c70215863532012a8a6d79b74496d0305b21ee18
Kolus <Kolus@192.168.137.189>
2013-12-22 22:47:50 +0400

    Создание Контрол и класса отключения команды для него

---
+++ src/kolyaezhenkovcommand/NoCommand.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kolyaezhenkovcommand;
+
+/**
+ *
+ * @author Kolus
+ */
+public class NoCommand implements Command {
+    @Override
+    public void execute() {}
+    @Override
+    public void undo() {}
+}
